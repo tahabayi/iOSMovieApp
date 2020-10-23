4c1ae5d7ce34dca978aec5d6b670fe784a4c1f60
Taha Metin Bayi <tahabayi@gmail.com>
2020-10-23 20:12:46 +0300

    Initialize the project.

---
+++ iOSMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSMovieApp
+//
+//  Created by Taha Metin Bayi on 23.10.2020.
+//  Copyright © 2020 Taha Metin Bayi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
